b5dea2daf6018afcc4612a882024c946fd070c0a
Marco Brunetti <151136910+marcocodd@users.noreply.github.com>
2024-01-30 17:04:36 +0100

    da sistemare ancora

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -7,19 +7,19 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 const AllTheBooks = () => (
- <Container>
-  <h2>Libri disponibili</h2>
+ <Container fluid className="bg-dark">
+  <h2 className="text-warning">Libri disponibili</h2>
   <Row className="g-3">
    {books.map((book) => {
     return (
      <Col xs={6} md={4} lg={2} key={book.asin}>
-      <Card className="h-100">
-       <Card.Img variant="top" src={book.img} />
+      <Card className="h-100 bg-black text-white">
+       <Card.Img className="img-fluid" variant="top" src={book.img} />
        <Card.Body>
         <Card.Title>{book.title}</Card.Title>
         <Card.Text>{book.category}</Card.Text>
         <Card.Text>{book.price}€</Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="warning">Go somewhere</Button>
        </Card.Body>
       </Card>
      </Col>
